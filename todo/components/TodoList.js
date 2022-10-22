24940a45a6d63d8d8792b54c981750a06b685585
Simron <simron0707@gmail.com>
2022-10-22 19:24:43 +0530

    frontend and smart contracts almost done

---
+++ todo/components/TodoList.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Navbar from './Navbar'
+import { IoMdAddCircle } from 'react-icons/io'
+
+const TodoList = () => <div id="mainContent" className='w-[80%] bg-gradient-to-l from-cyan-600 to-blue-800 py-4 px-9 rounded-[30px] overflow-y-scroll'>
+<Head>
+    <title>My ToDo List</title>
+    <meta name="keywords" content="web development ,programming"/>
+    </Head>
+  <Navbar />
+  <h2 className='text-4xl bolder text-white italic hover:not-italic pb-8'>
+    What&apos;s up, Simron!
+  </h2>
+  <div className='py-3 text-[#7d99e9] font-medium'>TODAY&apos;S TASKS</div>
+  <form className='flex items-center justify-center'>
+    <input
+      className='rounded-[10px] w-full p-[10px] border-none outline-none bg-[#031956] text-white mb-[10px]'
+      placeholder='Add a task for today...'
+      // take input from the form here
+    />
+    <IoMdAddCircle
+      // Add an onClick method
+      className='text-[#031956] text-[50px] cursor-pointer ml-[20px] mb-[10px]'
+    />
+  </form>
+  <ul>
+    {/* Loop through all tasks here using the Task component */}
+  </ul>
+</div>
+
+export default TodoList
---
+++ todo/pages/index.js
@@ -0,0 +1,42 @@
+import WrongNetworkMessage from '../components/WrongNetworkMessage'
+import ConnectWalletButton from '../components/ConnectWalletButton'
+import TodoList from '../components/TodoList'
+
+/* 
+const tasks = [
+  { id: 0, taskText: 'clean', isDeleted: false }, 
+  { id: 1, taskText: 'food', isDeleted: false }, 
+  { id: 2, taskText: 'water', isDeleted: true }
+]
+*/
+
+export default function Home() {
+
+  // Calls Metamask to connect wallet on clicking Connect Wallet button
+  const connectWallet = async () => {
+
+  }
+
+  // Just gets all the tasks from the contract
+  const getAllTasks = async () => {
+
+  }
+
+  // Add tasks from front-end onto the blockchain
+  const addTask = async e => {
+
+  }
+
+  // Remove tasks from front-end by filtering it out on our "back-end" / blockchain smart contract filtering doesn't mean deleting the todos
+  const deleteTask = key => async () => {
+
+  }
+
+  return (
+    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 h-screen w-screen flex justify-center py-4'>
+      {!'is user not logged in?' ? <ConnectWalletButton /> :
+        !'is this the correct network?' ? <TodoList /> : <WrongNetworkMessage />}
+    </div>
+  )
+}
+
